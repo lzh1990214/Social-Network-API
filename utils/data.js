a10fc8acbfb259cbb4f699b93f6204860b8e762c
zhihao li <zhihaoli@zhihaos-MBP.fios-router.home>
2023-03-27 23:39:03 -0400

    remove obsolete files

--- utils/data.js
+++ utils/data.js
@@ -1,55 +1,55 @@
-const userData = [
-    {
-        "username": "zhihaoli",
-        "email": "zhihaoli@gmail.com"
-    },
-    {
-        "username": "alexbaron",
-        "email": "alexbaron@gmail.com"
-    },
-    {
-        "username": "johndoe",
-        "email": "johndoe@gmail.com"
-    }
-];
+// const userData = [
+//     {
+//         "username": "zhihaoli",
+//         "email": "zhihaoli@gmail.com"
+//     },
+//     {
+//         "username": "alexbaron",
+//         "email": "alexbaron@gmail.com"
+//     },
+//     {
+//         "username": "johndoe",
+//         "email": "johndoe@gmail.com"
+//     }
+// ];
 
-const thoughtData = [
-    {
-        "thoughtText": "I just watched the new Marvel movie and it was amazing!",
-        "username": "zhihaoli",
-        "userId": ""
-    },
-    {
-        "thoughtText": "I'm so frustrated with the traffic in this city. It takes me forever to get anywhere!",
-        "username": "alexbaron",
-        "userId": ""
-    },
-    {
-        "thoughtText": "I just finished my first 10K race and I'm so proud of myself!",
-        "username": "johndoe",
-        "userId": ""
-    }
-];
+// const thoughtData = [
+//     {
+//         "thoughtText": "I just watched the new Marvel movie and it was amazing!",
+//         "username": "zhihaoli",
+//         "userId": ""
+//     },
+//     {
+//         "thoughtText": "I'm so frustrated with the traffic in this city. It takes me forever to get anywhere!",
+//         "username": "alexbaron",
+//         "userId": ""
+//     },
+//     {
+//         "thoughtText": "I just finished my first 10K race and I'm so proud of myself!",
+//         "username": "johndoe",
+//         "userId": ""
+//     }
+// ];
 
-const reactionData = [
-    {
-        "reactionBody": "Tell me about it. I try to avoid driving during rush hour as much as possible.",
-        "username": "zhihaoli",
-        "userId": "",
-        "thoughtId": ""
-    },
-    {
-        "reactionBody": "Really? I haven't seen it yet, but I'm excited to check it out now!",
-        "username": "johndoe",
-        "userId": "",
-        "thoughtId": ""
-    },
-    {
-        "reactionBody": "Congratulations! That's an amazing accomplishment.",
-        "username": "alexbaron",
-        "userId": "",
-        "thoughtId": ""
-    }
-];
+// const reactionData = [
+//     {
+//         "reactionBody": "Tell me about it. I try to avoid driving during rush hour as much as possible.",
+//         "username": "zhihaoli",
+//         "userId": "",
+//         "thoughtId": ""
+//     },
+//     {
+//         "reactionBody": "Really? I haven't seen it yet, but I'm excited to check it out now!",
+//         "username": "johndoe",
+//         "userId": "",
+//         "thoughtId": ""
+//     },
+//     {
+//         "reactionBody": "Congratulations! That's an amazing accomplishment.",
+//         "username": "alexbaron",
+//         "userId": "",
+//         "thoughtId": ""
+//     }
+// ];
 
 
